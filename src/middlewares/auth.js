89d74a95d193f510f4b933beb1055194351bf14e
René Morgado <renemorgado33@gmail.com>
2024-09-15 17:08:27 -0600

    feat(js): borrando logs

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,14 +2,12 @@ import jwt from 'jsonwebtoken';
 
 const checkAuth = (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1]; 
-    console.log(token)
     if (!token) {
         return res.status(401).json({ message: 'No token provided' });
     }
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
-        console.log('User authenticated', decoded);
         next();
     } catch (error) {
         return res.status(401).json({ message: 'Invalid token' });
